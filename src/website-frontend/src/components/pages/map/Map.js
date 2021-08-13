ae621d2253423c37c60836460fb1b811fbdaab16
Kasra <kasra76@gmail.com>
2021-08-13 05:57:56 +0430

    internal css styles has been cleaned & drawer inputs for development

--- src/website-frontend/src/components/pages/map/Map.js
+++ src/website-frontend/src/components/pages/map/Map.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap, useMemo, useCallback,  } from 'react-leaflet';
+import PropTypes from 'prop-types';
 // material ui components
 import clsx from 'clsx';
 import { alpha, makeStyles, useTheme } from '@material-ui/core/styles';
@@ -17,7 +18,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InputBase from '@material-ui/core/InputBase';
-import { Card, CardContent, Container, Fab, Input, Tab, Tabs, TextField } from '@material-ui/core';
+import { Card, CardContent, Container, Fab, Input, Paper, Tab, Tabs, TextField } from '@material-ui/core';
 // CSS
 import './map.css'
 // import 'leaflet/dist/leaflet.css'
@@ -35,40 +36,15 @@ import { FaWalking, FaWheelchair, FaTruck, FaCar, FaBiking } from 'react-icons/f
 import { GrLocationPin } from 'react-icons/gr'
 import { IoLocationSharp } from 'react-icons/io5'
 import { CgSandClock } from 'react-icons/cg'
-import { TabPanel } from '@material-ui/lab';
+import { RiPinDistanceFill } from 'react-icons/ri'
 
 
 const center = [51.505, -0.09]
 const zoom = 13
 
-const drawerWidth = 450;
+const drawerWidth = 410;
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    width: "30%",
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginTop: "8px",
-    marginRight: "8px",
-    borderRadius: "8px",
-    height: "48px"
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginRight: drawerWidth,
-  },
-  title: {
-    flexGrow: 1,
-  },
   hide: {
     display: 'none',
   },
@@ -183,7 +159,7 @@ const useStyles = makeStyles((theme) => ({
   tabs: {
     marginTop: "20px",
     borderLeft: `1px solid ${theme.palette.divider}`,
-    width: "100px",
+    width: "100px"
   },
   tabItem: {
     width: "100%",
@@ -191,21 +167,13 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "0px",
     minWidth: 0,
     letterSpacing: 0
-  }
+  },
 }));
-
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  };
-}
 
 export default function Map() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-  const [tabValue, setTabValue] = React.useState(0)
   
   const position = [51.505, -0.09] 
   
@@ -240,7 +208,6 @@ export default function Map() {
             />
           </div>
           <SearchIcon 
-            // color="action"
             className="search-icon"
           />
         </div>
@@ -253,12 +220,11 @@ export default function Map() {
           />
           <Marker position={position}>
             <Popup>
-              <BiCurrentLocation style={{fontSize: "24px"}}/>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
         </MapContainer>
-          {/* =================  map contents area ==============================*/}
+          {/* =================  my location button ==============================*/}
 
         <Fab
           aria-label="current location"
@@ -268,6 +234,8 @@ export default function Map() {
             className={classes.currentLocationIcon}
             />
         </Fab>
+
+          {/* ==================  Drawer   ==========================*/}
         <Drawer
           className={classes.drawer}
           variant="persistent"
@@ -299,43 +267,40 @@ export default function Map() {
               </IconButton>
             </div>
           </div>
-          {/* <Container className={classes.drawerBody}> */}
-                  {/* <div className="originInputContainer">
-                    <IoLocationSharp className="originIcon" />
-                    <input 
-                      className="originInput"
-                      placeholder="موقعیت شما"
-                      type="search"
-                    />
-                  </div>
-                  <div className="originInputContainer">
-                    <CgSandClock className="timeIcon" />
-                    <input 
-                      className="originInput"
-                      placeholder="موقعیت شما"
-                      type="search"
-                    />
-                  </div> */}
-                  <div className={classes.drawerBody}>
-                    <Tabs
-                      className={classes.tabs}
-                      variant="standard"
-                      orientation="vertical"
-                      value={tabValue}
-                      // onChange={}
-                      aria-label="measure methods tabs"
-                    >
-                      <Tab className={classes.tabItem} label="زمان" {...a11yProps(0)} />
-                      <Tab className={classes.tabItem} label="فاصله" {...a11yProps(1)} />
-                    </Tabs>
-                    <TabPanel value={tabValue} index={0}>
-                      Item One
-                    </TabPanel>
-                    <TabPanel value={tabValue} index={1}>
-                      Item Two
-                    </TabPanel>
-                  </div>
-          {/* </Container> */}
+          
+          {/* =============== Drawer body ====================== */}
+          <Container className={classes.drawerBody}>
+            <Card
+            color="green"
+            >
+              <CardContent>
+                <div className="originInputContainer">
+                  <IoLocationSharp className="originIcon" />
+                  <input 
+                    className="originInput"
+                    placeholder="موقعیت شما"
+                    type="search"
+                  />
+                </div>
+                <div className="originInputContainer">
+                  <CgSandClock className="timeIcon" />
+                  <input 
+                    className="originInput"
+                    placeholder="فاصله زمانی"
+                    type="search"
+                  />
+                </div>
+                <div className="originInputContainer">
+                  <RiPinDistanceFill className="timeIcon" />
+                  <input 
+                    className="originInput"
+                    placeholder="مسافت بر حسب متر"
+                    type="search"
+                  />
+                </div>
+              </CardContent>
+            </Card> 
+          </Container>
         </Drawer>
       </div>
     )
